a24af2a123b1adb8b794e1b2398166eda561394b
nqhung141196@gmail.com <quochung.nguyen@manabie.com>
2022-08-20 11:46:25 +0700

    update 2

--- insert_to_warehouse.py
+++ insert_to_warehouse.py
@@ -1,16 +1,20 @@
 import connect_db
 from sqlalchemy.orm import sessionmaker
 
+print('Input new item')
+while True:
+    StuffID = input('Stuff ID: ')
+    NameOfItem = input('Name of item: ')
+    Amount = input('Amount: ')
+    Category = input('Catergory of item:')
+    ask = input('Do you want input more? (Yes/No) ')
+    insert_new_item = connect_db.WareHouse(StuffID, NameOfItem, Amount, Category)
+    if ask == "No":
+        break
+
 Session = sessionmaker(bind=connect_db.engine)
 session = Session()
 
-StuffID = 'CV00002'
-NameOfItem = 'Co ve dau to 1.0mm'
-Amount = 3
-Category = 'Co ve'
-
-insert_new_item = connect_db.WareHouse(StuffID, NameOfItem, Amount, Category)
 session.add(insert_new_item)
-
 session.commit()
 session.close()
